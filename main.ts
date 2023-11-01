fa96a634bfa7495a8b05bf891046bdae21565f94
melvinpao-moe <122000946+melvinpao-moe@users.noreply.github.com>
2023-11-01 10:51:07 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,32 @@
-
+input.onGesture(Gesture.Shake, function () {
+    timer = 0
+    basic.showIcon(IconNames.Surprised)
+    basic.pause(1000)
+})
+let timer = 0
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    . # # # .
+    . . . . .
+    `)
+basic.forever(function () {
+    basic.pause(1000)
+    timer += 1
+    if (timer == 10) {
+        basic.showIcon(IconNames.Sad)
+        music.play(music.tonePlayable(175, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+    }
+    if (timer == 15) {
+        basic.showIcon(IconNames.Asleep)
+        music.play(music.tonePlayable(523, music.beat(BeatFraction.Half)), music.PlaybackMode.UntilDone)
+        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    }
+    if (timer == 20) {
+        music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Funeral), music.PlaybackMode.InBackground)
+        while (true) {
+            basic.showIcon(IconNames.Skull)
+        }
+    }
+})
